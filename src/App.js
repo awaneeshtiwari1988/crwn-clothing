f7fc79907d12cde0a9453c6ad27b7a0adb9dbe77
Awaneesh Tiwari <awaneesh@Awaneeshs-MacBook-Air.local>
2022-01-20 00:26:26 +0530

    Adding Shop Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
+      </Switch>
+    </BrowserRouter>
+        
     </div>
   );
 }
